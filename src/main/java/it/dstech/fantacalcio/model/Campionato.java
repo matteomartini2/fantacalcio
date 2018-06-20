3cb2a8ad353cb991eee02892a8ce75a108ffb335
lorenzo <lorenzo@lorenzo-HP-Pavilion-dv6-Notebook-PC>
2018-06-20 16:42:48 +0200

    Modifiche campionato

--- src/main/java/it/dstech/fantacalcio/model/Campionato.java
+++ src/main/java/it/dstech/fantacalcio/model/Campionato.java
@@ -2,61 +2,65 @@ package it.dstech.fantacalcio.model;
 
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity(name= "campionato")
 public class Campionato {
 
+	@Column(name="nome_campionato",nullable = false)
+	private String nomeCampionato;
 	
-	private String nome;
+	@Column(name="nazione_campionato",nullable = false)	
+	private String nazioneCampionato;
 	
-	private String nazione;
-	
+	@OneToMany
+	@JsonIgnore
 	private List<Squadra> listaSquadre;
 	
-	
+	@OneToMany
+	@JsonIgnore
 	private List<Giocatore> listaGiocatoriDisponibili;
 
 	
-
-	public String getNome() {
-		return nome;
+	
+	public String getNomeCampionato() {
+		return nomeCampionato;
 	}
 
-
-	public void setNome(String nome) {
-		this.nome = nome;
+	public void setNomeCampionato(String nomeCampionato) {
+		this.nomeCampionato = nomeCampionato;
 	}
 
-
-	public String getNazione() {
-		return nazione;
+	public String getNazioneCampionato() {
+		return nazioneCampionato;
 	}
 
-
-	public void setNazione(String nazione) {
-		this.nazione = nazione;
+	public void setNazioneCampionato(String nazioneCampionato) {
+		this.nazioneCampionato = nazioneCampionato;
 	}
-
 
 	public List<Squadra> getListaSquadre() {
 		return listaSquadre;
 	}
 
-
 	public void setListaSquadre(List<Squadra> listaSquadre) {
 		this.listaSquadre = listaSquadre;
 	}
-
 
 	public List<Giocatore> getListaGiocatoriDisponibili() {
 		return listaGiocatoriDisponibili;
 	}
 
-
 	public void setListaGiocatoriDisponibili(List<Giocatore> listaGiocatoriDisponibili) {
 		this.listaGiocatoriDisponibili = listaGiocatoriDisponibili;
 	}
+
+	
+
 	
 	
 }

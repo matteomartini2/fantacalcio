c256ea4ae3f0c61fef206d2a091010a7e05504f3
DanieleSmith <danielesmith92@gmail.com>
2018-06-21 10:08:34 +0200

    UserCtrl

--- src/main/java/it/dstech/fantacalcio/service/UserService.java
+++ src/main/java/it/dstech/fantacalcio/service/UserService.java
@@ -12,6 +12,31 @@ public class UserService {
 	@Autowired
 	private IUserRepository userRepository;
 	
+	public User create (User user) {
+		return userRepository.save(user);
+	}
+	public Iterable<User> createList (Iterable<User> listaUser){
+		return userRepository.saveAll(listaUser);
+	}
+	public Iterable<User> findAll() {
+		return userRepository.findAll();
+	}
+	public User findOne(Long id) throws Exception {
+		return userRepository.findById(id).orElseThrow(()-> new Exception());
+	}
+	public void deleteAll() {
+		userRepository.deleteAll();
+	}
+	public void deleteOne(Long id) {
+		userRepository.deleteById(id);
+	}
+	public User update (User userInput) throws Exception {
+		User userDb = findOne(userInput.getId());
+		userDb.setUsername(userInput.getUsername());
+		userDb.setPassword(userInput.getPassword());
+		return userRepository.save(userDb);
+	}
+	
 	public User findByUsername (String username) {
 		return userRepository.findByUsername(username);
 	}

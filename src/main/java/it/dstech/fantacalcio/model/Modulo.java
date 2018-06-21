7362d5793964cf9d62527874470e93b98b4c38b1
DanieleSmith <danielesmith92@gmail.com>
2018-06-21 15:26:40 +0200

    UserController

--- src/main/java/it/dstech/fantacalcio/model/Modulo.java
+++ src/main/java/it/dstech/fantacalcio/model/Modulo.java
@@ -3,5 +3,15 @@ package it.dstech.fantacalcio.model;
 public enum Modulo {
 
 	FORMAZIONE_UNO, FORMAZIONE_DUE, FORMAZIONE_TRE
+	
+	/*
+	 FORMAZIONE_UNO = 4-3-3
+	 
+	 
+	 FORMAZIONE_DUE = 4-2-3-1
+	 
+	 
+	 FORMAZIONE_TRE = 3-5-2
+	 */
 
 }
--- src/main/java/it/dstech/fantacalcio/service/SquadraService.java
+++ src/main/java/it/dstech/fantacalcio/service/SquadraService.java
@@ -91,4 +91,6 @@ public class SquadraService {
 
 		return dao.findBynome(nome);
 	}
+	
+	
 }

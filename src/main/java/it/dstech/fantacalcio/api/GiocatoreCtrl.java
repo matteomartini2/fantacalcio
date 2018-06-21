82f50100868e423f4ad37ffbdd5aec68efde0ad1
Cassia <cassia@192.168.1.108>
2018-06-21 10:38:46 +0200

    agg controller giocatore

--- src/main/java/it/dstech/fantacalcio/api/GiocatoreCtrl.java
+++ src/main/java/it/dstech/fantacalcio/api/GiocatoreCtrl.java
@@ -1,9 +1,17 @@
 package it.dstech.fantacalcio.api;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import it.dstech.fantacalcio.model.Giocatore;
+import it.dstech.fantacalcio.model.User;
 import it.dstech.fantacalcio.service.GiocatoreService;
 
 @RestController
@@ -13,5 +21,38 @@ public class GiocatoreCtrl {
 	@Autowired
 	private GiocatoreService service;
 	
+	@RequestMapping (method = RequestMethod.POST, value = "createOne")
+	public Giocatore createOne(Giocatore giocatore) {
+		return service.createOne(giocatore);
+	}
 	
+	@RequestMapping (method = RequestMethod.POST, value = "createLista")
+	public Iterable<Giocatore> createLista(ArrayList<Giocatore> listaGiocatori){
+		return service.createLista(listaGiocatori);
+	}
+	
+	@RequestMapping (method = RequestMethod.DELETE, value = "deleteOne")
+	public void deleteOne(Long id) {
+		service.deleteOne(id);
+	}
+	
+	@RequestMapping (method = RequestMethod.DELETE, value = "deleteAll")
+	public void deleteAll() {
+		service.deleteAll();
+	}
+	
+	@RequestMapping (method = RequestMethod.GET, value = "findOne")
+	public Optional<Giocatore> findOne(Long id) {
+		return service.findOne(id);
+	}
+	
+	@RequestMapping (method = RequestMethod.GET, value = "findAll")
+	public Iterable<Giocatore> findAll(){
+		return service.findAll();
+	}
+	
+	@RequestMapping (method = RequestMethod.PUT, value = "update")
+	public Giocatore update(Giocatore giocatoreInput, Long id) throws Exception {
+		return service.update(giocatoreInput, id);
+	}
 }

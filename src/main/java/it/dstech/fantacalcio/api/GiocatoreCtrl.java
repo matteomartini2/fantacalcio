2df282fd16ac213fa8dc3f1ac131819bcc543c6c
DanieleSmith <danielesmith92@gmail.com>
2018-06-27 11:44:57 +0200

    ultime modifiche

--- src/main/java/it/dstech/fantacalcio/api/GiocatoreCtrl.java
+++ src/main/java/it/dstech/fantacalcio/api/GiocatoreCtrl.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
--- src/main/java/it/dstech/fantacalcio/service/SquadraService.java
+++ src/main/java/it/dstech/fantacalcio/service/SquadraService.java
@@ -3,7 +3,6 @@ package it.dstech.fantacalcio.service;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
